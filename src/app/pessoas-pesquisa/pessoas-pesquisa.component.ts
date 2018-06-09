a2257a50f836fa258edaac54cf5933b512eeb90d
cmartinsgyn <cmartinsgyn@gmail.com>
2018-06-09 18:34:15 -0300

    aula 10.13 - pessoas

--- src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoasPesquisaComponent {
  pessoas = [
-   {nome: 'Cláudio Martins', cidade: 'Goiania', estado: 'GO', status: true },
-   {nome: 'Fulano da silva', cidade: 'Pirenópolis', estado: 'GO', status: true },
-   {nome: 'Paisano Folgado ', cidade: 'Manaus', estado: 'AM', status: true },
-   {nome: 'Marília Mendonça', cidade: 'Fortaleza', estado: 'CE', status: true },
-   {nome: 'Atanagildo Praxedes', cidade: 'Cuiabá', estado: 'MT', status: true },
-   {nome: 'Admastor Amancio', cidade: 'Raio que o parta', estado: 'TO', status: true },
-   {nome: 'Sinfrônio Amancio', cidade: 'Taboca do Brejo Velho', estado: 'BA', status: true },
-   {nome: 'Omoprogres Amancio', cidade: 'Toca do Coelho', estado: 'BA', status: true },
-   {nome: 'Helokit da Silva ', cidade: 'Gambiarra', estado: 'PE', status: true }
+   {nome: 'Cláudio Martins', cidade: 'Goiania', estado: 'GO', ativo: true },
+   {nome: 'Fulano da silva', cidade: 'Pirenópolis', estado: 'GO', ativo: true },
+   {nome: 'Paisano Folgado ', cidade: 'Manaus', estado: 'AM', ativo: true },
+   {nome: 'Marília Mendonça', cidade: 'Fortaleza', estado: 'CE', ativo: true },
+   {nome: 'Atanagildo Praxedes', cidade: 'Cuiabá', estado: 'MT', ativo: true },
+   {nome: 'Admastor Amancio', cidade: 'Raio que o parta', estado: 'TO', ativo: false },
+   {nome: 'Sinfrônio Amancio', cidade: 'Taboca do Brejo Velho', estado: 'BA', ativo: true },
+   {nome: 'Omoprogres Amancio', cidade: 'Toca do Coelho', estado: 'BA', ativo: true },
+   {nome: 'Helokit da Silva ', cidade: 'Gambiarra', estado: 'PE', ativo: false }
  ];
 
 }
