c45b0266384004c8aec2a341eaffaa271c81b4d3
claudio <cmartinsgyn@gmail.com>
2018-06-19 00:15:26 -0300

    aula 12.6 - two way data binding  para  obj cliente

--- src/app/formularios/form/form.component.ts
+++ src/app/formularios/form/form.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cliente {
+nome: string;
+email: string;
+profissao: string;
+}
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -8,12 +14,18 @@ import { NgForm } from '@angular/forms';
 })
 export class FormComponent {
 
+  cliente = new Cliente();
+
   profissoes = ['Programador', 'Empresario', 'Outra'];
   profissao = 'Outra';
 
   salvar(form: NgForm) {
-    console.log(this.profissao);
-    console.log(form.value.profissao);
+    //this.cliente.nome = form.value.nome;
+    //this.cliente.email = form.value.email;
+   // this.cliente.profissao = form.value.profissao;
+
+     console.log(form.value);
+     console.log(this.cliente);
   }
 
 }
