283386756cbc8f38821acc1da27ba7bdbac98949
cmartinsgyn <cmartinsgyn@gmail.com>
2018-06-28 11:48:19 -0300

    aula 13.10-desafio-form pessoa (end)

--- src/app/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,148 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pessoa-cadastro.component.html',
   styleUrls: ['./pessoa-cadastro.component.css']
 })
-export class PessoaCadastroComponent implements OnInit {
+export class PessoaCadastroComponent {
 
-  constructor() { }
+  estados = [{
+    value: 'AC',
+    label: 'Acre'
+  },
+       {
 
-  ngOnInit() {
+    value: 'AL',
+    label: 'Alagoas'
+  },
+       {
+
+    value: 'AM',
+    label: 'Amazonas'
+  },
+       {
+
+    value: 'AP',
+    label: 'Amapá'
+  },
+       {
+
+    value: 'BA',
+    label: 'Bahia'
+  },
+       {
+
+    value: 'CE',
+    label: 'Ceará'
+  },
+       {
+
+    value: 'DF',
+    label: 'Distrito Federal'
+  },
+       {
+
+    value: 'ES',
+    label: 'Espírito Santo'
+  },
+       {
+
+    value: 'GO',
+    label: 'Goiás'
+  },
+       {
+
+    value: 'MA',
+    label: 'Maranhão'
+  },
+       {
+
+    value: 'MG',
+    label: 'Minas Gerais'
+  },
+       {
+
+    value: 'MS',
+    label: 'Mato Grosso do Sul'
+  },
+       {
+
+    value: 'MT',
+    label: 'Mato Grosso'
+  },
+       {
+
+    value: 'PA',
+    label: 'Pará'
+  },
+       {
+
+    value: 'PB',
+    label: 'Paraíba'
+  },
+       {
+
+    value: 'PE',
+    label: 'Pernambuco'
+  },
+       {
+
+    value: 'PI',
+    label: 'Piauí'
+  },
+       {
+
+    value: 'PR',
+    label: 'Paraná'
+  },
+       {
+
+    value: 'RJ',
+    label: 'Rio de Janeiro'
+  },
+       {
+
+    value: 'RN',
+    label: 'Rio Grande do Norte'
+  },
+       {
+
+    value: 'RO',
+    label: 'Rondônia'
+  },
+       {
+
+    value: 'RR',
+    label: 'Roraima'
+  },
+       {
+
+    value: 'RS',
+    label: 'Rio Grande do Sul'
+  },
+       {
+
+    value: 'SC',
+    label: 'Santa Catarina'
+  },
+       {
+
+    value: 'SE',
+    label: 'Sergipe'
+  },
+       {
+
+    value: 'SP',
+    label: 'São Paulo'
+  },
+       {
+
+    value: 'TO',
+    label: 'Tocantins'}
+  ];
+
+  salvar(pessoaForm: NgForm) {
+    console.log(pessoaForm.value.nome);
+    console.log(pessoaForm);
+
   }
+
 
 }
