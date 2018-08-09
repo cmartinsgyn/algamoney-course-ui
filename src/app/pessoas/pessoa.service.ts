013be21f674ce76c8cad06e74d89bd8752a33eba
claudio <cmartinsgyn@gmail.com>
2018-08-09 00:27:08 -0300

    aula 17.7 {}

--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -1,5 +1,10 @@
+import { PessoaFiltro } from './pessoa.service';
 import { Injectable } from '@angular/core';
 import { Http, Headers, URLSearchParams } from '@angular/http';
+
+export class PessoaFiltro {
+ nome: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +17,13 @@ export class PessoaService {
   listarTodas(): Promise<any> {
     const headers = new Headers();
     // tslint:disable-next-line:max-line-length
-    headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm5vbWUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNTMzNTg0Nzk1LCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiI3MWE4ZjI0OC1kY2IyLTQ4MTctYTM1YS03Y2MxNGRjNjNkNmYiLCJjbGllbnRfaWQiOiJhbmd1bGFyIn0.WRMS6dEY4tvakiYXK2syIfbuA7fmWmyILiL5eJEej6o ');
+    headers.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbkBhbGdhbW9uZXkuY29tIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm5vbWUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNTMzNzg1OTc5LCJhdXRob3JpdGllcyI6WyJST0xFX0NBREFTVFJBUl9DQVRFR09SSUEiLCJST0xFX1BFU1FVSVNBUl9QRVNTT0EiLCJST0xFX1JFTU9WRVJfUEVTU09BIiwiUk9MRV9DQURBU1RSQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUEVTUVVJU0FSX0xBTkNBTUVOVE8iLCJST0xFX1JFTU9WRVJfTEFOQ0FNRU5UTyIsIlJPTEVfQ0FEQVNUUkFSX1BFU1NPQSIsIlJPTEVfUEVTUVVJU0FSX0NBVEVHT1JJQSJdLCJqdGkiOiJhNWE1Mzg3MC00ZjQyLTRjNWEtOWUzNS04YTgyMjg1NjU0MWUiLCJjbGllbnRfaWQiOiJhbmd1bGFyIn0.O1gFSPqGSsaepClKT7C362I-E703ycFXiFaht9MFuJM ');
 
     return this.http.get(`${this.pessoasUrl}?`, { headers })
     .toPromise()
     .then(response => response.json().content);
   }
 
-}
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+  return null;
+  }
