5797253f9c7b5bdad88f1573af430dd86271cf0a
Claudio <cmartinsgyn@gmail.com>
2018-08-25 09:18:09 -0300

    Aula 17.14 resolvendo o problema de não exibir msg personalizada

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -15,25 +15,47 @@ export class ErrorHandlerService {
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
 
-    } else if (errorResponse instanceof Response
-        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+    } else {
       let errors;
+      // deixei caso de algum erro no errors.
       msg = 'Ocorreu um erro ao processar a sua solicitação';
 
-      try {
         errors = errorResponse.json();
 
         msg = errors[0].mensagemUsuario;
-      } catch (e) { }
+
 
       console.error('Ocorreu um erro', errorResponse);
 
-    } else {
-      msg = 'Erro ao processar serviço remoto. Tente novamente.';
-      console.error('Ocorreu um erro', errorResponse);
     }
-
     this.toasty.error(msg);
   }
 
 }
+
+
+/*
+ if (typeof errorResponse === 'string') {
+  msg = errorResponse;
+
+}     *** não funciona essa verificação. ***
+ else if (errorResponse instanceof Response
+    && errorResponse.status >= 400 && errorResponse.status <= 499) {
+  let errors;
+  msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+  try {
+    errors = errorResponse.json();
+
+    msg = errors[0].mensagemUsuario;
+  } catch (e) { }
+
+  console.error('Ocorreu um erro', errorResponse);
+
+} else {
+  msg = 'Erro ao processar serviço remoto. Tente novamente.';
+  console.error('Ocorreu um erro', errorResponse);
+}
+
+this.toasty.error(msg);
+} */
