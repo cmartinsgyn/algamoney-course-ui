60efeb3508a5784c6a8fc21cfa549368eb2a1ad8
Marcos Fernando da Costa <marcos-fc@segplan.go.gov.br>
2018-11-05 18:57:52 -0200

    aula 19.14 - Tratamento p/ quando refresh token expirar (sessão expirou)

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 
 import { ToastyService } from 'ng2-toasty';
+import { NotAuthenticatedError } from '../seguranca/money-http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private toasty: ToastyService) { }
+  constructor(
+    private toasty: ToastyService,
+    private router: Router
+    ) { }
 
   handle(errorResponse: any) {
     let msg: string;
@@ -25,6 +30,10 @@ export class ErrorHandlerService {
 
   if (typeof errorResponse === 'string') {
     msg = errorResponse;
+
+  } else if (errorResponse instanceof NotAuthenticatedError) {
+    msg = 'Sua sessão expirou';
+    this.router.navigate(['/login']);
 
   } else if (errorResponse instanceof Response
       && errorResponse.status >= 400 && errorResponse.status <= 499) {
