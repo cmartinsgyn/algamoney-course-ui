21b78710a4d05210e8dddbb299c429f5ed7eebbd
Claudio <cmartinsgyn@gmail.com>
2018-11-10 23:53:30 -0200

    url para algamoney-api-cms.herokuapp.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
-  // apiUrl: 'https://algamoney-api-cms.herokuapp.com'
+  apiUrl: 'https://algamoney-api-cms.herokuapp.com'
 };
